386c905ff908b92ade93f26f86b7358e07e0db96
Hanif <hanif.aiubeee@gmail.com>
2022-12-03 00:13:26 +0600

    abc

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import React from 'react';
+import SearchOption from '../SearchOption/SearchOption';
+import Vector from '../../public/assets/images/Vector.png';
+import user from '../../public/assets/images/user.jpg'
+
+const Navbar = () => {
+    return (
+        <nav className=' h-16'>
+            <div className='flex items-center ml-6 lg:w-sidebar'>
+                <h2 className='mr-1.5'>Starbucks</h2>
+                <svg className='pl-px mt-1' width="10" height="8" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.59456 5.4382C3.79413 5.71473 4.20587 5.71473 4.40544 5.4382L7.75812 0.792603C7.99677 0.461921 7.76048 0 7.35267 0H0.647327C0.239523 0 0.00323534 0.461921 0.241885 0.792603L3.59456 5.4382Z" fill="#3E3F48" />
+                </svg>
+            </div>
+            <SearchOption />
+
+            <div className='flex mr-4'>
+                <Image src={Vector} alt='bell' />
+                <Image src={user} alt='user image' />
+
+                <h1 className='text-sm'>John Doe</h1>
+
+                <svg className='mt-1' width="10" height="8" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.59456 5.4382C3.79413 5.71473 4.20587 5.71473 4.40544 5.4382L7.75812 0.792603C7.99677 0.461921 7.76048 0 7.35267 0H0.647327C0.239523 0 0.00323534 0.461921 0.241885 0.792603L3.59456 5.4382Z" fill="#3E3F48" />
+                </svg>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'md': '800px',
+      'lg': '1440px',
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "neutral-2": "#F2F2F3",
+        "neutral-8": "#88898E"
+      },
+      width: {
+        'sidebar': '235px',
+        'logo': '110px',
+        'searchBar': '392px',
+      },
+    },
   },
   plugins: [],
 }
